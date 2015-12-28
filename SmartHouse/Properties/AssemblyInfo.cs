d10dbafa0b814561901e7db400c7b366901a99fa
z-valery <valera.z.211@yandex.ru>
2015-12-28 16:44:59 +0200

    v.1

--- SmartHouse/Properties/AssemblyInfo.cs
+++ SmartHouse/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85b784b3-0e8b-490b-8f2f-81acc7f0bc79")]
+[assembly: Guid("efca5c3f-a98b-4fb5-9046-1d5924d6a5c4")]
 
 // Version information for an assembly consists of the following four values:
 //
